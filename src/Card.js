5d6e460f8b72e45af734096df5a41341f34adcd5
dgasch512 <gaschdav208@gmail.com>
2020-07-25 19:40:13 -0500

    styling and cards added

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const Card = (props) => {
-  const { id, name, email} = props;
+const Card = ({ id, name, email}) => {
+
   return (
     // Return only one thing
     <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <h1>RoboFriends</h1>
       <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
