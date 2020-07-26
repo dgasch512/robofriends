b4a5baee5864e9f53e08d7593ae63439030e1043
dgasch512 <gaschdav208@gmail.com>
2020-07-26 15:33:36 -0500

    debugging

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ const store = createStore(searchRobots);
 ReactDOM.render(
                   <Provider store={store}>
                     <App />
-                  </Provider>, document.getElementById('root'));
+                  </Provider>, document.getElementById('root')
+                );
 
 
 // If you want your app to work offline and load faster, you can change
